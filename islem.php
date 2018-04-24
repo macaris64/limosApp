833e026f44c8bc494a1fb286a4c618f240a9a53c
Mehmet Acar <mehmetacar683@gmail.com>
2018-04-24 20:53:26 +0300

    Güncellendi(Yeşim Daştan)

--- islem.php
+++ islem.php
@@ -2,7 +2,31 @@
     include 'connect.php';
     ob_start();
     session_start();
-    if(isset($_POST['kullaniciGiris']))
+   
+    if(isset($_POST['kullaniciUyeOl']))
+    {
+        $name=$_POST['name'];
+        $password=$_POST['password'];
+        $repassword=$_POST['repassword'];
+        $email=$_POST['email'];
+        if ($password == $repassword)
+        {
+            $statement = $conn->prepare("INSERT INTO kullanici(name, password, email)
+    VALUES(:fname, :pass, :email)");
+            $statement->execute(array(
+                "fname" => $name,
+                "pass" => $password,
+                "email" => $email
+            ));
+        }
+        else
+        {
+            header("Location:welcome.php?durum=no");
+        }
+
+
+    }
+ if(isset($_POST['kullaniciGiris']))
     {
         $email = $_POST['email'];
         $password =  $_POST['password'];
@@ -24,29 +48,6 @@
         {
             header("Location:welcome.php?durum=no");
             exit;
-        }
-
-
-    }
-    if(isset($_POST['kullaniciUyeOl']))
-    {
-        $name=$_POST['name'];
-        $password=$_POST['password'];
-        $repassword=$_POST['repassword'];
-        $email=$_POST['email'];
-        if ($password == $repassword)
-        {
-            $statement = $conn->prepare("INSERT INTO kullanici(name, password, email)
-    VALUES(:fname, :pass, :email)");
-            $statement->execute(array(
-                "fname" => $name,
-                "pass" => $password,
-                "email" => $email
-            ));
-        }
-        else
-        {
-            header("Location:welcome.php?durum=no");
         }
 
 
